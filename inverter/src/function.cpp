9854f2a59debcddb700c25e157ca5de56372488b
Selan <selan.santos@ufrn.br>
2021-06-10 22:03:46 -0300

    first commit

---
+++ inverter/src/function.cpp
@@ -0,0 +1,11 @@
+#include "function.h"
+
+/*! 
+ * Reverse de order of elements inside the array.
+ * @param arr Reference to the array with the values.
+ */
+template <size_t SIZE>
+void reverse( std::array< std::string, SIZE > & arr )
+{
+    // TODO: Coloque aqui sua resposta.
+}
---
+++ negativos5/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+#include <algorithm>
+using std::min;
+
+const int SIZE = 5; // input size.
+
+int main(void)
+{
+    // TODO: Adicione aqui seu código.
+
+    return 0;
+}
